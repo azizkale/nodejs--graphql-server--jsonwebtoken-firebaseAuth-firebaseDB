751bce0a8770ac834f9426b2ef965c93da804b7f
kale1st <devmath1st@hotmail.com>
2022-04-18 05:12:27 +0200

    apolllo-express and ejs added

--- src/index.js
+++ src/index.js
@@ -1,11 +1,33 @@
-const { ApolloServer } = require('apollo-server');
+const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
 const resolvers = require("./schema/resolvers");
 const typeDefs = require("./schema/typeDefs");
-const dotenv = require("dotenv");
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const PORT = process.env.PORT || 5000;
 
-// The `listen` method launches a web server.
-server.listen().then(({ url }) => {
-    console.log(`🚀  Server ready at ${url}`);
-});
+async function startApolloServer() {
+    const app = express();
+    const server = new ApolloServer({
+        typeDefs,
+        resolvers,
+    });
+    await server.start();
+
+    server.applyMiddleware({ app });
+    app.set('view engine', 'ejs');
+    app.get('/login', (req, res) => {
+        res.render('../src/view/index')
+    })
+    app.use((req, res) => {
+        res.status(200);
+        res.send('Hello!');
+
+        res.end();
+    });
+
+
+    await new Promise(resolve => app.listen(PORT, resolve));
+    console.log(`Server running on port http://localhost:${PORT}${server.graphqlPath}`)
+    return { server, app };
+}
+startApolloServer();
