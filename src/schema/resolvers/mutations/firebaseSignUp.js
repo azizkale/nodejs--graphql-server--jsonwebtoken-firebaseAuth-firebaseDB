cdf8f04ecc10708b6ca6035e3f3a2fcd8ff91e9d
kale1st <devmath1st@hotmail.com>
2022-04-28 04:53:39 +0200

    signUp fixed

--- src/schema/resolvers/mutations/firebaseSignUp.js
+++ src/schema/resolvers/mutations/firebaseSignUp.js
@@ -1,29 +1,49 @@
 const { getAuth, createUserWithEmailAndPassword } = require("firebase/auth");
 const firebaseApp = require("../../../tools/firebaseTools");
-import createUser from '../../../funcitons/createUser';
-const { v1: uuidv1, v4: uuidv4 } = require('uuid');
+import { getDatabase, ref, set } from "firebase/database";
 
-const signUpFirebase = async(_, { email, password, groupId }) => {
+const signUpFirebase = async(_, { email, password, name, groupId, groupname }) => {
 
     const auth = await getAuth(firebaseApp);
     let accessToken;
+    //creating and saving user to firebase authenticate
     await createUserWithEmailAndPassword(auth, email, password).
     then(async(userCredential) => {
         // Signed in 
         accessToken = await userCredential.user.stsTokenManager.accessToken;
 
-        //Create User To Reatime DB
-        const userId = await uuidv1();
-        // const user = await new User(userId, name, email, groupId);
-        await createUser(email, groupId);
+        //getting userId from Firebase Authentication
+        let userId;
+        await auth.onAuthStateChanged((user) => {
+            if (user) {
+                // User logged in already or has just logged in.
+                userId = user.uid;
+            } else {
+                // User not logged in or has just logged out.
+            }
+        });
+
+        //saving to firebaseDB
+        const db = await getDatabase();
+        await set(ref(db, 'groups/' + groupId + '/members/' + userId), {
+            name: name,
+            email: email,
+            userId: userId,
+            groupname: groupname
+        });
+
         return accessToken;
     }).catch((error) => {
+        const errorCode = error.code;
         const errorMessage = error.message;
         return errorMessage;
     });
+
     if (accessToken == null)
-        return null
+        return "Please try again"
     else return accessToken;
+
+
 }
 
 module.exports = signUpFirebase;
