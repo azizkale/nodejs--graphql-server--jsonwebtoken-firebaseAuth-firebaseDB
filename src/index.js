0d97ab015b40774282ba3bb88a436f3b2c7dfb98
azizkale <azizkale@hotmail.com>
2022-07-02 19:58:14 +0200

    context func worls well

--- src/index.js
+++ src/index.js
@@ -1,55 +1,18 @@
 const { ApolloServer } = require('apollo-server');
 const resolvers = require("./schema/resolvers");
 const typeDefs = require("./schema/typeDefs");
-import jwt from "jsonwebtoken";
-import { rule, shield, and, or, not } from "graphql-shield";
-import secretkey from "./tools/keypair";
+
 
 const PORT = process.env.PORT || 5000;
-
-const getClaims = (req) => {
-    let token;
-    try {
-        // token = jwt.verify(req.request.headers.authorization, secretkey);
-        token = jwt.verify(req.req.rawHeaders[13], secretkey);
-
-    } catch (e) {
-        return null;
-    }
-    return token;
-}
-
-
-// Rules
-const isAuthenticated = rule()(async(parent, args, ctx, info) => {
-
-    return ctx.claims !== null;
-});
-
-const canAddUser = rule()(async(parent, args, ctx, info) => {
-    return ctx.claims.role === "admin";
-});
-
-
-// Permissions
-const permissions = shield({
-    Query: {
-        hello: and(isAuthenticated),
-        signInFirebase: and(isAuthenticated),
-    },
-    Mutation: {
-        signUpFirebase: and(isAuthenticated),
-    },
-});
-
-
 const server = new ApolloServer({
     typeDefs,
     resolvers,
-    middlewares: [permissions],
-    context: (req) => ({
-        claims: getClaims(req),
-    }),
+    csrfPrevention: true,
+    cache: 'bounded',
+    context: ({ req }) => {
+        const token = req.headers.authorization || '';
+        return { token };
+    },
 });
 
 server.listen({ port: PORT }).then(() => {
--- src/schema/resolvers/query/hello.js
+++ src/schema/resolvers/query/hello.js
@@ -1,4 +1,12 @@
-const hello = () => {
-    return "hello world";
+import jwt from "jsonwebtoken";
+
+
+const hello = (_, {}, context) => {
+    const ctx = jwt.decode(context.token);
+
+    if (ctx.user.role === "mentor")
+        return "hello world";
+    else
+        return "not authorized"
 }
 module.exports = hello;
